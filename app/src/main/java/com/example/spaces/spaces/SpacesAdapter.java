b10e95e9519e833bb4b57090982ef670909030f2
Matthew Zyle <mjz49@case.edu>
2018-03-15 22:41:17 -0400

    Started CardView implementation (incomplete)

--- app/src/main/java/com/example/spaces/spaces/SpacesAdapter.java
+++ app/src/main/java/com/example/spaces/spaces/SpacesAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.support.v7.widget.CardView;
 
 /**
  * Created by Steven on 3/14/2018.
@@ -21,6 +22,7 @@ public class SpacesAdapter extends RecyclerView.Adapter<SpacesAdapter.ViewHolder
         // each data item is just a string in this case
         //We will probably want more views to be here (like a CardView maybe?)
         public TextView mTextView;
+        public CardView mCardView;
         public ViewHolder(View v) {
             super(v);
             mTextView = v.findViewById(R.id.spaceTextView);
@@ -28,6 +30,7 @@ public class SpacesAdapter extends RecyclerView.Adapter<SpacesAdapter.ViewHolder
             //Sound example code I found used below line to set some stuff for listening to clicks
             //mTextView.setOnClickListener(this);
         }
+
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
